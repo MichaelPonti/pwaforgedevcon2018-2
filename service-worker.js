455a008e02a28b42dab5d3d4b48fc390f0a46972
Michael Ponti <mike.ponti@honeywell.com>
2018-10-15 21:51:11 -0700

    update for github.io

--- service-worker.js
+++ service-worker.js
@@ -9,8 +9,8 @@ const SHELL_CACHE_NAME_PREFIX = 'app-shell-';
 const SHELL_CACHE_NAME = SHELL_CACHE_NAME_PREFIX + '023';
 
 
-const SERVER_PREFIX = '/';
-// const SERVER_PREFIX = '/pwaforgedevcon2018-2/';
+// const SERVER_PREFIX = '/';
+const SERVER_PREFIX = '/pwaforgedevcon2018-2/';
 
 
 var shellFilesToCache = [
